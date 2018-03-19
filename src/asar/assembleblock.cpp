1fd7e60fe542a9a9d165816fd39d2b77ebb0c638
randomdude999 <just.so.you.can.email.me@gmail.com>
2018-03-19 18:03:26 +0200

    Add a warning for using ;@xkas and warn xkas
    As it says, I didn't actually remove any xkas compatiblility features, I
    only made using @xkas/warn xkas throw a warning saying it's deprecated.

--- src/asar/assembleblock.cpp
+++ src/asar/assembleblock.cpp
@@ -1089,6 +1089,7 @@ void assembleblock(const char * block)
 		if (!asarverallowed) error(0, "This command may only be used at the start of a file.");
 		if (incsrcdepth != 1 && !emulatexkas) error(0, "This command may only be used in the root file.");
 		if (specifiedasarver) error(0, "Using @xkas and @asar in the same patch is not supported.");
+		warn0("xkas support is being deprecated. Please use an older version of Asar (<=1.50) if you need it.");
 		emulatexkas=true;
 		optimizeforbank=0x100;
 		checksum=false;
@@ -1910,7 +1911,10 @@ void assembleblock(const char * block)
 		else if (!stricmp(word[2], "off")) val=false;
 		else error(0, "Invalid warn command.");
 		if(0);
-		else if (!stricmp(word[1], "xkas")) warnxkas=val;
+		else if (!stricmp(word[1], "xkas")) {
+			warn0("xkas support is being deprecated. Please use an older version of Asar (<=1.50) if you need it.");
+			warnxkas=val;
+		}
 		else error(0, "Invalid warn command.");
 	}
 	else if (is0("fastrom"))
