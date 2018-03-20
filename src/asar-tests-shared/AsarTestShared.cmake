cb75da3cd6e9e6963987ab5e5d726cd7dc745a4b
randomdude999 <just.so.you.can.email.me@gmail.com>
2018-03-20 16:42:18 +0200

    Make automatic inline expansion not throw a warning

--- src/asar-tests-shared/AsarTestShared.cmake
+++ src/asar-tests-shared/AsarTestShared.cmake
@@ -14,9 +14,10 @@ macro(set_asar_test_shared_properties target)
 		endif()		
 		
 		# These certainly aren't worth a warning, though
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4514")
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4710")
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4820")
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4514") # unreferenced inline function removed
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4710") # function not inlined
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4711") # function selected for automatic inline expansion
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4820") # 'bytes' bytes padding added after construct 'member_name'
 	else()
 		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -pedantic")
 		target_link_libraries(${target} dl)
