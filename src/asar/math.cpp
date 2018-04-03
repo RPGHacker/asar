42667abd03a971ccbfe461eb3a37fecdb255aeb2
randomdude999 <just.so.you.can.email.me@gmail.com>
2018-04-03 18:31:29 +0300

    i think this is a better way to round a double

--- src/asar/math.cpp
+++ src/asar/math.cpp
@@ -760,7 +760,7 @@ static double getnumcore()
 static double sanitize(double val)
 {
 	if (val!=val) error("NaN encountered.");
-	if (math_round) return (unsigned int)val;
+	if (math_round) return round(val); // why did this use an int cast?
 	return val;
 }
 
