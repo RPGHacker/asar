eb0832e949c3eb5e845cec6e91cff98d56451781
randomdude999 <just.so.you.can.email.me@gmail.com>
2018-04-03 23:25:56 +0300

    Remove useless files (#60)
    * remove 2 useless files
    
    * restructure tests (merge asar-app-test and asar-dll-test), mostly rewrite test cmake
    
    * update travis and appveyor config
    
    * update readme

--- make.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#For all Linux users out there. I don't think anything uses the Asar library on Linux, but it does no harm.
-g++ -std=c++98 -Wall -Werror -Wno-unused-result -Wno-uninitialized *.cpp -oasar -g -fno-rtti -DINTERFACE_CLI -Dstricmp=strcasecmp -Dlinux -DRELEASE
-g++ -std=c++98 -fPIC -Wall -Werror -Wno-unused-result -Wno-uninitialized *.cpp -olibasar.so -g -fno-rtti -DINTERFACE_LIB -Dstricmp=strcasecmp -Dlinux -DRELEASE -shared
