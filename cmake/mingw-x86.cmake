393ed484adfcc585b89e48d9dd64bdb165cc03e3
boldowa <boldowa.q@gmail.com>
2018-06-23 02:50:12 +0900

    MinGW cross builds

---
+++ cmake/mingw-x86.cmake
@@ -0,0 +1,5 @@
+#-----------------------------------------------------------
+# mingw x86 toolchain
+#-----------------------------------------------------------
+set(MINGW_TYPE "i686-w64-mingw32")
+include(${CMAKE_CURRENT_LIST_DIR}/_mingw-common.cmake)
