8c74c9674cedeecf55a2817131fb97e749b7e391
David Crooks <davidacrooks@outlook.com>
2018-12-20 19:48:57 -0500

    Changing #ifdef/#def/#endif in crc32.h to #pragma once

--- src/asar/crc32.h
+++ src/asar/crc32.h
@@ -2,9 +2,6 @@
 * Generic crc32 helper function
 */
 
-#ifndef __CRC32_H__
-#define __CRC32_H__
+#pragma once
 
 extern unsigned long crc32(const unsigned char *s, unsigned int len);
-
-#endif /* __CRC32_H__ */
