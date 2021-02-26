06ec38e02412040a76890bb3148993bd05a10b3d
Atari2 <alex.robassomero@gmail.com>
2021-02-26 14:28:08 +0100

    asar doesn't crash anymore

--- src/asar/fopen_helper.cpp
+++ src/asar/fopen_helper.cpp
@@ -3,8 +3,8 @@ FILE* wfopen(const char* handle, const char* mode) {
 	#ifdef _WIN32
   wchar_t *wideHandle = new wchar_t[strlen(handle) + 1];
   wchar_t *wideMode = new wchar_t[strlen(mode) + 1];
-  mbstowcs(wideHandle, handle, strlen(handle));
-  mbstowcs(wideMode, mode, strlen(mode));
+  mbstowcs(wideHandle, handle, strlen(handle) + 1);
+  mbstowcs(wideMode, mode, strlen(mode) + 1);
   FILE *file = _wfopen(wideHandle, wideMode);
   delete[] wideHandle;
   delete[] wideMode;
