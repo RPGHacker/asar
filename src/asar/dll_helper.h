37b9f03dbe8e311803566610b6bf8e1da7a0c1aa
p4plus2 <p4plus2@gmail.com>
2021-03-15 22:53:42 -0700

    Potential clang work around.
    How many layers of indirection do we need here >.>

--- src/asar/dll_helper.h
+++ src/asar/dll_helper.h
@@ -2,41 +2,45 @@
 
 #include <windows.h>
 
+struct function_pointer_wrapper/*have this struct at global level*/
+{
+	static void (*callback)(void *);
+	static void __stdcall execute(void *parameter) { return callback(parameter); }
+};
+
 template <typename functor> 
 bool run_as_fiber(functor &&callback) {
-    struct fiber_wrapper {
-        functor &callback;
-        void *original;
-        bool result;
+	struct fiber_wrapper {
+		functor &callback;
+		void *original;
+		bool result;
 
-        void execute() {
-            result = callback();
-            SwitchToFiber(original);
-        }
+		void execute() {
+			result = callback();
+			SwitchToFiber(original);
+		}
 
-    } wrapper{callback, nullptr, false};
+	} wrapper{callback, nullptr, false};
 
-    auto fiber = CreateFiberEx(
-        4 * 1024 * 1024, 8 * 1024 * 1024, 0,
-        [](void *parameter) {
-          reinterpret_cast<fiber_wrapper *>(parameter)->execute();
-        },
-        &wrapper);
+	function_pointer_wrapper::callback = [](void *parameter){
+		reinterpret_cast<fiber_wrapper*>(parameter)->execute();
+	};
+	auto fiber = CreateFiberEx(4*1024*1024, 8*1024*1024, 0, &function_pointer_wrapper::execute, &wrapper);
 
-    if (!fiber) {
-        return callback();
-    }
+	if (!fiber) {
+		return callback();
+	}
 
-    void* main_thread = ConvertThreadToFiber(nullptr);
-    if (!main_thread && GetLastError() != ERROR_ALREADY_FIBER) { 
-            return callback();
-    }
-    wrapper.original = GetCurrentFiber();
-    SwitchToFiber(fiber);
-    DeleteFiber(fiber);
-    if (main_thread) {
-        ConvertFiberToThread();
-    }
-    return wrapper.result;
+	void* main_thread = ConvertThreadToFiber(nullptr);
+	if (!main_thread && GetLastError() != ERROR_ALREADY_FIBER) { 
+		return callback();
+	}
+	wrapper.original = GetCurrentFiber();
+	SwitchToFiber(fiber);
+	DeleteFiber(fiber);
+	if (main_thread) {
+		ConvertFiberToThread();
+	}
+	return wrapper.result;
 }
 #endif
