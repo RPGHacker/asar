2e24afbda259e29dc12b1c287b1ab9984c561e3f
RPG Hacker <markus_wall@web.de>
2022-06-17 18:18:54 +0200

    Renamed "asar-standalone" output file to "asar"

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 ABS=$(pwd)
 mkdir tests_tmp_dll tests_tmp_app
 $ABS/asar-tests/asar-dll-test $ABS/asar/libasar.so $ABS/tests $ABS/dummy_rom.sfc $ABS/tests_tmp_dll
-$ABS/asar-tests/asar-app-test $ABS/asar/asar-standalone $ABS/tests $ABS/dummy_rom.sfc $ABS/tests_tmp_app
+$ABS/asar-tests/asar-app-test $ABS/asar/asar $ABS/tests $ABS/dummy_rom.sfc $ABS/tests_tmp_app
