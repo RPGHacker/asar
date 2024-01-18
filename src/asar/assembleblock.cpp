80f123e62c2a319ad21ff2e96d75811625289b1d
RPG Hacker <markus_wall@web.de>
2024-01-18 09:32:17 +0100

    Deprecated "if a = b"

--- src/asar/assembleblock.cpp
+++ src/asar/assembleblock.cpp
@@ -1104,7 +1104,11 @@ void assembleblock(const char * block, bool isspecialline)
 					else if (!strcmp(nextword[1], "<"))  thiscond=(par1<par2);
 					else if (!strcmp(nextword[1], ">=")) thiscond=(par1>=par2);
 					else if (!strcmp(nextword[1], "<=")) thiscond=(par1<=par2);
-					else if (!strcmp(nextword[1], "="))  thiscond=(par1==par2);
+					else if (!strcmp(nextword[1], "="))
+					{
+						asar_throw_warning(0, warning_id_feature_deprecated, "if a = b", "use ""if a == b"" instead");
+						thiscond=(par1==par2);
+					}
 					else if (!strcmp(nextword[1], "==")) thiscond=(par1==par2);
 					else if (!strcmp(nextword[1], "!=")) thiscond=(par1!=par2);
 					//else if (!strcmp(nextword[1], "<>")) thiscond=(par1!=par2);
