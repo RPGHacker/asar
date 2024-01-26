2157909b638d854bfa48b93578350dd9fc1c41fe
trillian <trillian.git@r9.pm>
2024-01-26 16:36:43 +0200

    minor sectionhack.js fixes

--- docs/newbook/sectionhack.js
+++ docs/newbook/sectionhack.js
@@ -6,11 +6,13 @@
         for(let section of document.querySelectorAll("h2 .header")) {
             let list_el = document.createElement("li");
             list_el.className = "chapter-item expanded";
-            list_el.append(section.cloneNode(true));
+            let new_link = section.cloneNode(true);
+            new_link.insertAdjacentHTML("afterbegin", `<strong aria-hidden="true">#</strong> `);
+            list_el.append(new_link);
             the_list.append(list_el);
         }
         let list_wrap = document.createElement("li");
-        list_wrap.className = "chapter-item expanded";
+        list_wrap.className = "expanded";
         list_wrap.append(the_list);
         thispage.parentElement.insertAdjacentElement("afterend", list_wrap);
     }
