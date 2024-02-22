00a9a5f946a71e7bfc2176fdc3ba73b0afc8f8ad
trillian <trillian.git@r9.pm>
2024-02-22 21:33:21 +0200

    add relevances to syntax highlighter

--- docs/newbook/theme/highlight.js
+++ docs/newbook/theme/highlight.js
@@ -408,22 +408,31 @@ variants:[{match:[/\bclass/,/\s+/,a,/\s*/,/\(\s*/,a,/\s*\)/]},{
 match:[/\bclass/,/\s+/,a]}],scope:{1:"keyword",3:"title.class",
 6:"title.class.inherited"}},{className:"meta",begin:/^[\t ]*@/,end:/(?=#)|$/,
 contains:[g,m,b]}]}}})();hljs.registerLanguage("python",e)})();
-hljsAsar = {
-    init: function() {
+window.hljsAsar = {
+    init: function(enableObsolete) {
         // I know this is ugly, but I don't know how else to solve Asar's label rules...
         const asarOpcodes = ["db", "dw", "dl", "dd", "adc", "and", "asl", "bcc", "blt", "bcs", "bge", "beq", "bit", "bmi", "bne", "bpl", "bra", "brk", "brl", "bvc", "bvs", "clc", "cld", "cli", "clv", "cmp", "cop", "cpx", "cpy", "dec", "dea", "dex", "dey", "eor", "inc", "ina", "inx", "iny", "jmp", "jml", "jsr", "jsl", "lda", "ldx", "ldy", "lsr", "mvn", "mvp", "nop", "ora", "pea", "pei", "per", "pha", "phb", "phd", "phk", "php", "phx", "phy", "pla", "plb", "pld", "plp", "plx", "ply", "rep", "rol", "ror", "rti", "rtl", "rts", "sbc", "sec", "sed", "sei", "sep", "sta", "stp", "stx", "sty", "stz", "tax", "tay", "tcd", "tcs", "tdc", "trb", "tsc", "tsb", "tsx", "txa", "txs", "txy", "tya", "tyx", "wai", "wdm", "xba", "xce", "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15", "add", "alt1", "alt2", "alt3", "asr", "bic", "cache", "cmode", "color", "div2", "fmult", "from", "getb", "getbh", "getbl", "getbs", "getc", "hib", "ibt", "iwt", "ldb", "ldw", "link", "ljmp", "lm", "lms", "lmult", "lob", "loop", "merge", "mult", "not", "or", "plot", "ramb", "romb", "rpix", "sbk", "sex", "sm", "sms", "stb", "stop", "stw", "sub", "swap", "to", "umult", "with", "xor", "addw", "ya", "and1", "bbc0", "bbc1", "bbc2", "bbc3", "bbc4", "bbc5", "bbc6", "bbc7", "bbs0", "bbs1", "bbs2", "bbs3", "bbs4", "bbs5", "bbs6", "bbs7", "call", "cbne", "clr0", "clr1", "clr2", "clr3", "clr4", "clr5", "clr6", "clr7", "clrc", "clrp", "clrv", "cmpw", "daa", "das", "dbnz", "decw", "di", "div", "ei", "eor1", "incw", "mov", "sp", "mov1", "movw", "mul", "not1", "notc", "or1", "pcall", "pop", "push", "ret", "reti", "set0", "set1", "set2", "set3", "set4", "set5", "set6", "set7", "setc", "setp", "sleep", "subw", "tcall", "tclr", "tset", "xcn", "lea", "move", "moves", "moveb", "movew"];
 
-        const asarKeywords = ["lorom", "hirom", "exlorom", "exhirom", "sa1rom", "fullsa1rom", "sfxrom", "norom", /* "macro", */ "endmacro", "struct", "endstruct", "extends", "incbin", "incsrc", "fillbyte", "fillword", "filllong", "filldword", "fill", "padbyte", "pad", "padword", "padlong", "paddword", "cleartable", "skip", "namespace", "print", "org", "base", "on", "off", "reset", "freespaceuse", "pc", "bytes", "hex", "freespace", "freespace_settings", "freecode", "freedata", "ram", "noram", "align", "noalign", "cleaned", "nocleaned", "static", "nostatic", "autoclean", "prot", "pushpc", "pullpc", "pushbase", "pullbase", "function", "if", "else", "elseif", "endif", "while", "endwhile", "for", "endfor", "assert", "arch", "65816", "spc700", "superfx", "bankcross", "full", "half", "bank", "noassume", "auto", "asar", "includefrom", "includeonce", "include", "error", "double", "pushtable", "pulltable", "undef", "check", "title", "nested", "warnings", "push", "pull", "disable", "enable", "warn", "address", "dpbase", "optimize", "dp", "none", "always", "default", "mirrors", "global", "spcblock", "endspcblock", "nspc", "custom", "execute", "offset", "pushns", "pullns", "segment", "start", "pin", "rats", "norats", "freespacebyte"];
+        let asarKeywords = ["exlorom", "exhirom", "sa1rom", "fullsa1rom", "sfxrom", "norom", /* "macro", */ "endmacro", "struct", "endstruct", "extends", "incbin", "incsrc", "fillbyte", "fillword", "filllong", "filldword", "fill", "padbyte", "pad", "padword", "padlong", "paddword", "cleartable", "skip", "namespace", "print", "org", "base", "on", "off", "reset", "freespaceuse", "pc", "bytes", "hex", "freespace_settings", "ram", "noram", "align", "noalign", "cleaned", "nocleaned", "static", "nostatic", "autoclean", "prot", "pushpc", "pullpc", "pushbase", "pullbase", "function", "if", "else", "elseif", "endif", "while", "endwhile", "for", "endfor", "assert", "arch", "65816", "spc700", "superfx", "bankcross", "full", "half", "bank", "noassume", "auto", "asar", "includefrom", "includeonce", "include", "error", "double", "pushtable", "pulltable", "undef", "check", "title", "nested", "warnings", "push", "pull", "disable", "enable", "warn", "address", "dpbase", "optimize", "dp", "none", "always", "default", "mirrors", "global", "spcblock", "endspcblock", "nspc", "custom", "execute", "offset", "pushns", "pullns", "segment", "start", "pin", "rats", "norats", "freespacebyte"];
+
+        const asarRelevantKeywords = ["lorom", "hirom", "freespace", "freecode", "freedata"];
+
+        // the old syntax highlighter also included "import" but i don't think that's ever been an asar keyword??
+        const asarObsoleteKeywords = ["xkas", "math", "autoclear", "pri", "table", "ltr", "rtl", "round", "warnpc", "inline", "raw"];
 
         const asarIntrinsicFunctions = ["read1", "read2", "read3", "read4", "canread1", "canread2", "canread4", "sqrt", "sin", "cos", "tan", "asin", "acos", "atan", "arcsin", "arccos", "arctan", "log", "log10", "log2", "readfile1", "readfile2", "readfile3", "readfile4", "canreadfile1", "canreadfile2", "canreadfile3", "canreadfile4", "canreadfile", "filesize", "getfilestatus", "snestopc", "pctosnes", "max", "min", "clamp", "safediv", "select", "not", "equal", "notequal", "less", "lessequal", "greater", "greaterequal", "and", "or", "nand", "nor", "xor", "defined", "sizeof", "objectsize", "stringsequal", "stringsequalnocase"];
+
+        if(enableObsolete === true) {
+            asarKeywords = asarKeywords.concat(asarObsoleteKeywords);
+        }
 
         const asarNumberLiteralsMode = {
             scope: "number",
             variants:
             [
-                { begin: /(?<=\W|^)[0-9]+(\.[0-9]+)?(?=\W|$)/ },
-                { begin: /(?<=\W|^)%[0-1]+(?=\W|$)/ },
-                { begin: /(?<=\W|^)\$[0-9a-fA-F]+(?=\W|$)/ },
+                { begin: /(?<=\W|^)[0-9]+(\.[0-9]+)?(?=\W|$)/, relevance: 0 },
+                { begin: /(?<=\W|^)%[0-1]+(?=\W|$)/, relevance: 2 },
+                { begin: /(?<=\W|^)\$[0-9a-fA-F]+(?=\W|$)/, relevance: 2 },
             ],
         };
         const asarOperatorsMode = {
@@ -519,6 +528,7 @@ hljsAsar = {
                                 { begin: "'", end: /'|$/ },
                                 { begin: '"', end: /"|$/ },
                             ],
+                            relevance: 0,
                             contains: [
                                 { begin: /\\(!|\\)/ },
                                 asarVariadicMacroArg, asarSimpleMacroArg,
@@ -535,7 +545,8 @@ hljsAsar = {
                             contains: [
                                 { scope: "keyword", begin: "macro "},
                                 { scope: "title.macro", begin: /[a-zA-Z_][a-zA-Z0-9_]*/ },
-                            ]
+                            ],
+                            relevance: 5,
                         },
                         {
                             scope: "title.macro",
@@ -549,11 +560,18 @@ hljsAsar = {
                         asarLabelDefinitionMode,
                         {
                             scope: "keyword",
-                            begin: asarKeywords.join('\\b|') + "\\b"
+                            begin: asarRelevantKeywords.join('\\b|') + "\\b",
+                            relevance: 10,
+                        },
+                        {
+                            scope: "keyword",
+                            begin: asarKeywords.join('\\b|') + "\\b",
+                            relevance: 3,
                         },
                         {
                             scope: "built_in",
-                            begin: asarOpcodes.join('(\\.[bwl]|\\b)|') + '(\\.[bwl]|\\b)'
+                            begin: asarOpcodes.join('(\\.[bwl]|\\b)|') + '(\\.[bwl]|\\b)',
+                            relevance: 5,
                         },
                         //asarLabelDefinitionMode,
                         asarBracedDefineOutsideMacro,
@@ -569,4 +587,7 @@ hljsAsar = {
     }
 };
 
-hljsAsar.init();
+// window.hljsAsar.init();
+// or window.hljsAsar.init(true); to include obsolete keywords
+
+window.hljsAsar.init();
