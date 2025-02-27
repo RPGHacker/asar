e46cafafc4f929873444e493c9ea0bac7b92939b
trillian <trillian.git@r9.pm>
2025-02-27 19:00:26 +0200

    uglify new test (todo revert later)

--- tests/math_fail.asm
+++ tests/math_fail.asm
@@ -1,25 +1,25 @@
 ;`errEdivision_by_zero
+;`errEnan
+;`errEnan
+;`errEnegative_shift
+;`errEnegative_shift
+;`errEoob
+;`errEoob
+;`errEargument_count
+;`errEargument_count
+;`errEargument_count
+;`errEfile_offset_out_of_bounds
+;`errEfile_offset_out_of_bounds
 print dec(1/0)
-;`errEnan
 print dec(log(-0.5))
-;`errEnan
 print dec(sqrt(-1))
-;`errEnegative_shift
 assert 1 << -1
-;`errEnegative_shift
 assert 1 >> -1
-;`errEoob
 assert char("helloworld", -5)
-;`errEoob
 assert char("helloworld", 10)
-;`errEargument_count
 assert sqrt(1,2,3)
-;`errEargument_count
 assert sqrt()
-;`errEargument_count
 assert read1($1234, $42, 123)
 
-;`errEfile_offset_out_of_bounds
 assert readfile1("data/filename with spaces.bin", -1)
-;`errEfile_offset_out_of_bounds
 assert readfile1("data/filename with spaces.bin", 16)
