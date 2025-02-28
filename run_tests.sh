a1c952a261f38344a63ab316d558790e5b101184
trillian <trillian.git@r9.pm>
2025-02-28 05:32:35 +0200

    delete run_tests.sh/bat
    these were out of date already, were made completely useless by the last
    commit, and are superseded by the run-tests cmake target

--- run_tests.sh
+++
@@ -1,4 +0,0 @@
-ABS=$(pwd)
-mkdir tests_tmp_dll tests_tmp_app
-$ABS/asar-tests/asar-dll-test $ABS/asar/lib/libasar.so $ABS/tests $ABS/dummy_rom.sfc $ABS/tests_tmp_dll
-$ABS/asar-tests/asar-app-test $ABS/asar/bin/asar $ABS/tests $ABS/dummy_rom.sfc $ABS/tests_tmp_app
